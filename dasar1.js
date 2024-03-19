1079af29356425a8b950fd72400771b916b32e6e
Aldo <aldoreyhansavero21@gmail.com>
2024-03-19 13:31:29 +0700

    Push Part3

---
+++ dasar1.js
@@ -0,0 +1,27 @@
+let nama = "Vinny";
+let umur = "22";
+let apakahorang = false;
+let uang = 1000.5;
+
+
+let angka1 = 10;
+let angka2 = 20;
+
+console.log(angka1 + angka2, "Tambah");
+console.log(angka1 - angka2, "Kurang");
+console.log(angka1 / angka2, "Bagi");
+console.log(angka1 * angka2, "Perkalian");
+console.log(angka1 % angka2, "Modulus");
+
+let hasil = angka1 + angka2
+console.log(hasil)
+
+angka1 = 30;
+angka2 = 10;
+console.log(angka1 + angka2, "Berubah");
+
+console.log(apakahorang + 10 + true);
+
+const hewan = "kucing";
+// hewan = "anjing"; error Assignment to constant variable.
+console.log(hewan)
---
+++ ml.js
@@ -0,0 +1,15 @@
+let mabar = "";
+switch (Math.floor(Math.random() * 6)) {
+    case 2:
+      mabar = "Lu Duo";
+      break;
+    case 3:
+      mabar = "Lu Trio";
+      break;
+    case 5:
+       mabar = "Lu Mabar 5";
+      break;
+    default:
+        mabar = "P Mabar";
+  }
+  console.log(mabar)
